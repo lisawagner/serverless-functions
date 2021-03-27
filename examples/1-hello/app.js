71b27c51e9876705da61507b395b8402de7aa401
lisawagner <lisawagner.online@gmail.com>
2021-03-27 15:34:20 -0700

    initial setup

---
+++ examples/1-hello/app.js
@@ -0,0 +1,11 @@
+const result = document.querySelector(".result");
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get("/.netlify/functions/1-hello");
+    result.textContent = data;
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+fetchData();
